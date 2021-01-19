901e821ee446c971e212a90d611c2a0042ae77e3
kelter-js <spawnkillerqw@gmail.com>
2021-01-19 19:43:53 +0400

    Автоматизация сборки

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,56 @@ const csso = require("postcss-csso");
 const rename = require("gulp-rename");
 const htmlminify = require("gulp-htmlmin");
 const jsminify = require("gulp-uglify-es");
-const imageminify = require("gulp-imagemin");
 const webp = require("gulp-webp");
 const svgstore = require("gulp-svgstore");
 const del = require("del");
 const imagemin = require("gulp-imagemin");
 const sync = require("browser-sync").create();
+
+//HTML
+
+const htmlminify = () => {
+  return gulp.src("source/*.html")
+  .pipe(htmlminify({collapseWhitespace: true}))
+  .pipe(gulp.dest("build"))
+}
+
+exports.htmlminify = htmlminify;
+
+//Scripts
+
+const scripts = () => {
+  return gulp.src("source/js/script.js")
+  .pipe(jsminify())
+  .pipe(rename("script.min.js"))
+  .pipe(gulp.dest("build/js"))
+  .pipe(sync.stream());
+}
+
+exports.scripts = scripts;
+
+//Copy
+
+const copy = () => {
+  return gulp.src([
+    "source/fonts/*.{woff2,woff}",
+    "source/img/**/*.{jpg,png,svg}"
+  ],
+  {
+    base: "source",
+  })
+  .pipe(guld.dest("build"));
+}
+
+exports.copy = copy;
+
+//Clean
+
+const clean = () => {
+  return del("build");
+}
+
+exports.clean = clean;
 
 //Images
 
@@ -42,7 +86,7 @@ const styles = () => {
     ]))
     .pipe(sourcemap.write("."))
     .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
@@ -54,15 +98,17 @@ const sprite = () => {
   return gulp.src("source/img/icons/*.svg")
   .pipe(svgstore())
   .pipe(rename("main-sprite.svg"))
-  .pipe(gulp.dest("build/img/icons"))
+  .pipe(gulp.dest("build/img"))
 }
+
+exports.sprite = sprite;
 
 //Webp
 
 const createWebp = () => {
-  return gulp.src("source/img**/*{jpg,png}")
+  return gulp.src("source/img/**/*.{jpg,png}")
   .pipe(webp({quality: 90}))
-  .pipe(gulp.dest("build"))
+  .pipe(gulp.dest("build/img"))
 }
 
 exports.createWebp = createWebp;
@@ -72,7 +118,7 @@ exports.createWebp = createWebp;
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -86,10 +132,40 @@ exports.server = server;
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/less/**/*.less", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload);
+  gulp.watch("source/less/**/*.less", gulp.series(styles));
+  gulp.watch("source/js/script.js", gulp.series(scripts));
+  gulp.watch("source/*.html", gulp.series(htmlminify, sync.reload));
 }
 
+//Build
+
+const build = gulp.series(
+  clean,
+  gulp.parallel(
+    styles,
+    htmlminify,
+    scripts,
+    sprite,
+    copy,
+    images,
+    createWebp
+  )
+)
+
+exports.build = build;
+
 exports.default = gulp.series(
-  styles, server, watcher
+  clean,
+  gulp.parallel(
+    styles,
+    htmlminify,
+    scripts,
+    sprite,
+    copy,
+    createWebp
+  ),
+  gulp.series(
+    server,
+    watcher
+  )
 );
