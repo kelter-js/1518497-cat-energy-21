c58188c57e747a40beec33596fdd06fc8d44f46c
kelter-js <spawnkillerqw@gmail.com>
2021-01-20 21:09:27 +0400

    Реализовал логику слайдера, спозиционировал кота для таблетки и десктопа в секции с примером.

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,37 @@
 let mainMenu = document.querySelector(".main-nav");
 let mainMenuButton = document.querySelector(".main-nav__toggle");
+let interactiveMap = document.querySelector(".dealers__map-container");
+let staticMap = document.querySelector(".dealers__map-wrapper");
+let sliderImageBefore = document.querySelector(".example__image--before");
+let sliderImageAfter = document.querySelector(".example__image--after");
+let sliderButtonsContainer = document.querySelector(".example__slider-controls-wrapper");
+
+if(sliderButtonsContainer) {
+  sliderButtonsContainer.addEventListener("click", function(evt) {
+    let target = evt.target;
+    if(target.classList.contains("example__slider-text--before")) {
+      if(sliderImageBefore.classList.contains("example__image--hidden")) {
+        sliderImageBefore.classList.toggle("example__image--hidden");
+        sliderImageAfter.classList.toggle("example__image--hidden");
+      }
+    }
+    if(target.classList.contains("example__slider-text--after")) {
+      if(sliderImageAfter.classList.contains("example__image--hidden")) {
+        sliderImageBefore.classList.toggle("example__image--hidden");
+        sliderImageAfter.classList.toggle("example__image--hidden");
+      }
+    }
+  })
+}
+
 if(mainMenu.classList.contains("main-nav--withoutjs")) {
   mainMenu.classList.remove("main-nav--withoutjs")
 }
+
 mainMenuButton.addEventListener("click", function() {
   mainMenu.classList.toggle('main-nav--closed');
   mainMenu.classList.toggle('main-nav--opened');
 });
-
-let interactiveMap = document.querySelector(".dealers__map-container");
-let staticMap = document.querySelector(".dealers__map-wrapper");
 
 if(interactiveMap.classList.contains("invisible")) {
   interactiveMap.classList.toggle("invisible");
