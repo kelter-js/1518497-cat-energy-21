4b277696f82ada8cedf3dbf41cb227ac1ceee9a2
kelter-js <spawnkillerqw@gmail.com>
2021-01-24 10:49:57 +0400

    Правка классов по бэм, фикс изменения высоты секции из-за переключения слайда на мобильной версии.

--- source/js/script.js
+++ source/js/script.js
@@ -2,8 +2,8 @@ let mainMenu = document.querySelector(".main-nav");
 let mainMenuButton = document.querySelector(".main-nav__toggle");
 let interactiveMap = document.querySelector(".dealers__map-container");
 let staticMap = document.querySelector(".dealers__map-wrapper");
-let sliderImageBefore = document.querySelector(".example__image--before");
-let sliderImageAfter = document.querySelector(".example__image--after");
+let sliderImageBefore = document.querySelector(".example__image-before");
+let sliderImageAfter = document.querySelector(".example__image-after");
 let sliderButtonsContainer = document.querySelector(".example__slider-controls-wrapper");
 let sliderIndicator = document.querySelector(".example__slider-toggle");
 
@@ -11,17 +11,17 @@ if(sliderButtonsContainer) {
   sliderButtonsContainer.addEventListener("click", function(evt) {
     let target = evt.target;
     if(target.classList.contains("example__slider-text--before")) {
-      if(sliderImageBefore.classList.contains("example__image--hidden")) {
-        sliderImageAfter.classList.toggle("example__image--hidden");
-        sliderImageBefore.classList.toggle("example__image--hidden");
+      if(sliderImageBefore.classList.contains("example__image-hidden")) {
+        sliderImageAfter.classList.toggle("example__image-hidden");
+        sliderImageBefore.classList.toggle("example__image-hidden");
         sliderIndicator.classList.toggle("example__slider-toggle--after");
         sliderIndicator.classList.toggle("example__slider-toggle--before");
       }
     }
     if(target.classList.contains("example__slider-text--after")) {
-      if(sliderImageAfter.classList.contains("example__image--hidden")) {
-        sliderImageBefore.classList.toggle("example__image--hidden");
-        sliderImageAfter.classList.toggle("example__image--hidden");
+      if(sliderImageAfter.classList.contains("example__image-hidden")) {
+        sliderImageBefore.classList.toggle("example__image-hidden");
+        sliderImageAfter.classList.toggle("example__image-hidden");
         sliderIndicator.classList.toggle("example__slider-toggle--after");
         sliderIndicator.classList.toggle("example__slider-toggle--before");
       }
